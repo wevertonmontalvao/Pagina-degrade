ac43b1768b01445943e51f2dc10209a82ae218be
Weverton Montalvão <104617134+wevertonmontalvao@users.noreply.github.com>
2023-07-27 16:48:34 -0300

    Add files via upload

---
+++ arquivo-js/index.js
@@ -0,0 +1,16 @@
+window.br = ScrollReveal({ reset: true }); 
+
+br.reveal('.area-1',{
+    rotate: { x:50, y: 70, z:80 },
+        duration: 1000
+})
+
+br.reveal('.area-2',{
+    rotate: { x:0, y:80, z:0 }, 
+        duration: 2000
+})
+
+br.reveal('.area-3',{
+    rotate: { x:70, y:80, z:0 }, 
+        duration: 2000
+})
